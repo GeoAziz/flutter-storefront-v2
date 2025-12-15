cc6be427d7d8142791efee2080e9b6c34b2c8d74
GeoAziz <mishaelelvis@zetech.ac.ke>
2025-12-15 04:21:55 -0500

    [#13] Wire Home: PopularProducts to productRepositoryProvider (minimal mapping)

--- lib/screens/home/views/components/popular_products.dart
+++ lib/screens/home/views/components/popular_products.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shop/components/product/product_card.dart';
-import 'package:shop/models/product_model.dart';
 import 'package:shop/route/route_names.dart';
+import 'package:shop/providers/repository_providers.dart';
 
 import '../../../../constants.dart';
 
-class PopularProducts extends StatelessWidget {
+class PopularProducts extends ConsumerWidget {
   const PopularProducts({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final repo = ref.read(productRepositoryProvider);
+    final future = repo.fetchProducts();
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -23,34 +27,38 @@ class PopularProducts extends StatelessWidget {
             style: Theme.of(context).textTheme.titleSmall,
           ),
         ),
-        // While loading use 👇
-        // const ProductsSkelton(),
         SizedBox(
           height: 220,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            // Find demoPopularProducts on models/ProductModel.dart
-            itemCount: demoPopularProducts.length,
-            itemBuilder: (context, index) => Padding(
-              padding: EdgeInsets.only(
-                left: defaultPadding,
-                right: index == demoPopularProducts.length - 1
-                    ? defaultPadding
-                    : 0,
-              ),
-              child: ProductCard(
-                image: demoPopularProducts[index].image,
-                brandName: demoPopularProducts[index].brandName,
-                title: demoPopularProducts[index].title,
-                price: demoPopularProducts[index].price,
-                priceAfetDiscount: demoPopularProducts[index].priceAfetDiscount,
-                dicountpercent: demoPopularProducts[index].dicountpercent,
-                press: () {
-                  Navigator.pushNamed(context, RouteNames.productDetails,
-                      arguments: index.isEven);
-                },
-              ),
-            ),
+          child: FutureBuilder<List>(
+            future: future,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              final products = snapshot.data ?? [];
+              return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: products.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.only(
+                    left: defaultPadding,
+                    right: index == products.length - 1 ? defaultPadding : 0,
+                  ),
+                  child: ProductCard(
+                    image: '',
+                    brandName: '',
+                    title: products[index].title,
+                    price: 0.0,
+                    priceAfetDiscount: null,
+                    dicountpercent: null,
+                    press: () {
+                      Navigator.pushNamed(context, RouteNames.productDetails,
+                          arguments: index.isEven);
+                    },
+                  ),
+                ),
+              );
+            },
           ),
         )
       ],
